b008568d98f4fffb3d7177181e0bd07082365448
aansubarkah <aansubarkah@gmail.com>
2015-08-21 11:07:34 +0700

    add map

---
+++ app/pods/markers/route.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	setupController: function(controller) {
+		controller.setProperties({
+			lat: 32.75494243654723,
+			lng: -86.8359375,
+			zoom: 4
+		});
+	}
+});
--- app/router.js
+++ app/router.js
@@ -2,14 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-	location: config.locationType,
-	rootURL: config.baseURL
+    location: config.locationType,
+    rootURL: config.baseURL
 });
 
 Router.map(function () {
-	//this.resource('weathers');
-	this.route('weathers');
-	this.route('weather');
+  this.route('weathers');
+  this.route('weather');
+  this.route('markers');
+  this.route('marker');
 });
 
 export default Router;

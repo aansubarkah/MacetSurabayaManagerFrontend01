c41955541ec56158c9c9c4180eace58800db939a
aansubarkah <aansubarkah@gmail.com>
2015-09-03 23:12:59 +0700

    1st upload

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -13,7 +13,8 @@ export default DS.RESTAdapter.extend({
 		return false;
 	},
 	namespace: 'manager',
-	host: 'http://localhost:8765',// @todo change this on production server
+	host: 'http://apitraffic.aansubarkah.net',// @todo change this on production server
+	//host: 'http://localhost:8765',// @todo change this on production server
 	ajax: function (url, method, hash) {
 		//"Accept": "application/json",
 		//"Content-Type": "application/json"
